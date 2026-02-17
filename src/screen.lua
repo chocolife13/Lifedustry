29bd3b9951654d1266df6253b954db31d36457fc
chocolife13 <carte.evolbie@gmail.com>
2026-02-17 10:15:03 +0000

    finish the function in screen

--- src/screen.lua
+++ src/screen.lua
@@ -1,5 +1,10 @@
 screen = {}
+
+
+
 function screen.relative_to_percent(x, y)
-    return x, y
+    screen.width, screen.height = love.graphics.getPixelDimensions()
+    return (x / 100)* screen.width, (y / 100)* screen.height
 end
+
 return screen

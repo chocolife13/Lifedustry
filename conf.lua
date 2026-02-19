e03324474f7e223728baa7948b721d0d813d21c4
SoffQoff Word <83302448+SoffQoffWord@users.noreply.github.com>
2026-02-19 11:13:31 +0100

    VGA bro
    Changed window dimensions to 640x480.

--- conf.lua
+++ conf.lua
@@ -1,8 +1,9 @@
 function love.conf(t)
 	t.window.title = "Lidustry V0.0.0" -- the main windows title
-	t.window.width = 1280      
-  t.window.height = 720
+	t.window.width = 640	      
+  t.window.height = 480
   t.console = true -- activate the console
   t.window.resizable = true -- allow the windows to be resizable
   t.window.vsync = 0 -- V-Sync
 end
+

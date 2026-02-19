cc6df63272666e36ae0ac9fd787cb179d6c4199f
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-02-19 10:07:16 +0000

    set a defalt resolution in conf.lua

--- conf.lua
+++ conf.lua
@@ -1,6 +1,8 @@
 function love.conf(t)
 	t.window.title = "Lidustry V0.0.0" -- the main windows title
-	t.console = true -- activate the console
+	t.window.width = 1280      
+  t.window.height = 720
+  t.console = true -- activate the console
   t.window.resizable = true -- allow the windows to be resizable
   t.window.vsync = 0 -- V-Sync
 end

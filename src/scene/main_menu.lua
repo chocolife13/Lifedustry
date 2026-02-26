254d0fabb230f2f143206839c0d220ec61feb3be
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-02-26 22:49:02 +0000

    button work but i can't use scene function bc scenemanager load the main scene

--- src/scene/main_menu.lua
+++ src/scene/main_menu.lua
@@ -3,7 +3,14 @@ local assets = require("src.assets")
 local ui = require("src.display.ui")
 local gui = require("src.display.gui")
 
+
 local api = {}
+
+function butonclick_test()
+    scene_manager.scene = "loading"
+    scene_manager.load()
+end
+
 
 function api.load()
     
@@ -11,7 +18,7 @@ end
 
 function api.draw()
     ui.print_centered("Lifedustry", screen.relative_to_percent.width(50), screen.relative_to_percent.height(15))
-    gui.button("If u tryed to click ur a fuckin idiot", 50, 50, 250, 35, print("bro literaly think this button work"))
+    gui.button("If u tryed to click ur a fuckin idiot", 50, 50, 250, 35, function() butonclick_test() end)
     
 end
 

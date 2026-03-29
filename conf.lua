802204f703e3607d1e5dc221be0efb91fbfa307d
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-03-29 14:43:59 +0200

    base

--- conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-	t.window.title = "Lifedustry" -- the main windows title
-	t.window.width = 640	      
-  t.window.height = 480
-  t.console = true -- activate the console
-  t.window.resizable = true -- allow the windows to be resizable
-  t.window.vsync = 0 -- V-Sync
-end
-

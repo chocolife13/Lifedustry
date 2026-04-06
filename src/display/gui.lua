f12b3fb50789555fa282513ebfe2f2087fab23f7
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-04-06 10:57:53 +0200

    a menu functional and fix gui spam

--- src/display/gui.lua
+++ src/display/gui.lua
@@ -8,11 +8,12 @@ function api.button(text, x, y, width, height, callback)
     local x = screen.relative_to_percent.width(x) - width / 2
     local y = screen.relative_to_percent.height(y) - height / 2
     local mx, my = love.mouse.getPosition()
+    local button = 1
     local isHover = (mx > (x) and mx < (x) + width and my > (y) and my < (y) + height)
-    local isClicked = isHover and love.mouse.isDown(1)
-    if isClicked then  
-        callback()
-        
+    if isHover then
+        function love.mousereleased(x, y, button)
+            callback()
+        end
     end
     ui.print_centered(text, x + width / 2, y + height / 2)
     love.graphics.rectangle("line", x , y, width, height)

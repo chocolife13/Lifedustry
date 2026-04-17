e989a4d5e10347610906c16d400374a2b24aaf6b
Chocolife13 on termux:x11 tablet <no@mgeneric_mail.site>
2026-04-17 18:47:30 +0200

    dev arg and actual grass with beatiful (not creepy) man

--- main.lua
+++ main.lua
@@ -6,6 +6,9 @@ local args_manager = require("src.args_manager")
 
 function love.load(args)
     args_manager.start_scene(args)
+    if dev then
+        print("debug mod activated")
+    end
     assets.preload()
     scene_manager.load() -- start the actual scene
 end
@@ -13,10 +16,16 @@ end
 
 function love.update(dt) -- la partie mise à jour
     scene_manager.update(dt) 
+    if love.keyboard.isDown("f") then
+        love.window.showMessageBox("A info from your keeboard", "Have you pressed f ?", info)
+    end
 end
 
 
 function love.draw() -- la partie graphique
     scene_manager.draw() 
-    love.graphics.print(love.timer.getFPS(), 10, 10)
+    if dev then
+        love.graphics.print("scene: " .. scene_manager.scene, 10, 10)
+        love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 30)
+    end
 end

f8ff174a506c93199aae9ed55897c6406375e9df
Chocolife13 on termux:x11 tablet <no@mgeneric_mail.site>
2026-04-18 14:04:01 +0200

    bro unlock the map plain map instead of the oneblock map

--- src/scene/game.lua
+++ src/scene/game.lua
@@ -13,7 +13,12 @@ function api.load()
 end
 
 function api.draw()
-    love.graphics.draw(assets.textures.block.grass, 50, 50)
+    for ix = 0, 10 do
+        for yx = 0, 10 do
+            love.graphics.draw(assets.textures.block.grass, ix * 64, yx * 64)
+        end
+    end
+    
     love.graphics.draw(assets.textures.player, screen.relative_to_percent.width(50), screen.relative_to_percent.height(50))
 
     love.graphics.setBackgroundColor(0, 0, 0, 0)

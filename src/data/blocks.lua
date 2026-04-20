97702224e551b26bca84ee42204dabae15dc3a7a
Chocolife13 on termux:x11 tablet <no@mgeneric_mail.site>
2026-04-20 12:13:27 +0200

    no i don't steal gimp texture and yes i add void block

--- src/data/blocks.lua
+++ src/data/blocks.lua
@@ -1,3 +1,5 @@
 api = {}
+api.void = {name = "void"}
 api.grass = {name = "grass", texture = "assets.textures.block.grass"}
+api.stone = {name = "stone", texture = "assets.textures.block.stone"}
 return api

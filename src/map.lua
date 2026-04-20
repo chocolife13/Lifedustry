21a0c254ffed1375252bd0e09da567fc479b0bae
chocolife13 <carte.evolbie@gmail.com>
2026-04-20 12:47:09 +0200

    bro unlock new block, be quiet!! the world is generating

--- src/map.lua
+++ src/map.lua
@@ -3,9 +3,13 @@ local blocks = require("src.data.blocks")
 api = {}
 
 function api.draw()
-    for ix = 0, 10 do
-        for yx = 0, 10 do
-            love.graphics.draw(blocks.grass.texture, ix * 64, yx * 64)
+    for ix = 0, 20 do
+        for yx = 0, 20 do
+            if love.math.noise(ix/20, yx/20) > 0.5 then
+                love.graphics.draw(blocks.grass.texture, ix * 64, yx * 64)
+            else
+                love.graphics.draw(blocks.stone.texture, ix * 64, yx * 64)
+            end
         end
     end
 end

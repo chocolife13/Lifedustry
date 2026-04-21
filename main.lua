cd1065aae10778a84bcbeacaab50c45d18427090
chocolife13 <carte.evolbie@gmail.com>
2026-04-21 13:10:28 +0200

    better generation and devmode info is in debug

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 scene_manager = require("src.scene.scene_manager")
 assets = require("src.assets")
+debug = require("src.core.debug")
 local args_manager = require("src.args_manager")
 
 -- Ces trois fonctions constituent la boucle principale du jeu
@@ -26,11 +27,5 @@ end
 
 function love.draw() -- la partie graphique
     scene_manager.draw() 
-    if dev then
-        love.graphics.print("scene: " .. scene_manager.scene, 10, 10)
-        love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 30)
-        love.graphics.print(string.format("RAM: %.1f MB", collectgarbage("count") / 1024), 10, 50)
-        love.graphics.print(string.format("VRAM: %.1f MB", love.graphics.getStats().texturememory / (1024* 1024)), 10, 70)
-        love.graphics.print(string.format("DrawCalls: %d", love.graphics.getStats().drawcalls), 10, 90)
-    end
+    if dev then debug.info() end
 end 

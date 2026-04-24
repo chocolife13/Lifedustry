a9deeed55f044d6269af4f4be5db3a07ca0fa170
chocolife13 <carte.evolbie@gmail.com>
2026-04-24 18:20:53 +0200

    Water by me and Grass by AdrianTrain09

--- src/data/biome.lua
+++ src/data/biome.lua
@@ -12,8 +12,6 @@ function api.sand(x, y)
     love.graphics.draw(blocks.sand.texture, x * 64, y * 64)
 end
 function api.water(x, y)
-    love.graphics.setColor(0, 0, 1)
-    love.graphics.rectangle("fill", x * 64, y * 64, 64, 64)
-    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(blocks.water.texture, x * 64, y * 64)
 end
 return api
--- src/data/blocks.lua
+++ src/data/blocks.lua
@@ -4,4 +4,5 @@ api.void = {name = "void"}
 api.grass = {name = "grass", texture = assets.textures.block.grass}
 api.stone = {name = "stone", texture = assets.textures.block.stone}
 api.sand = {name = "sand", texture = assets.textures.block.sand}
+api.water = {name = "water", texture = assets.textures.block.water}
 return api

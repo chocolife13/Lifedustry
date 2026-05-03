545685afb4eaf1c6f8658048ae3cba6c70dae8d8
chocolife13 <carte.evolbie@gmail.com>
2026-05-03 13:22:25 +0200

    set default scene in scene manager

--- src/args_manager.lua
+++ src/args_manager.lua
@@ -2,22 +2,19 @@ local scene_manager = require("src.scene.scene_manager")
 local api = {}
 
 function api.start_scene(args)
-    -- valeur par default
-    local scene = "loading"
     dev = false
 
     -- loop to see each args
     for i=1, #args do
         
-        if args[i] == "--scene" and args[i+1] then -- si args scene et un args apres
-            scene = args[i+1]
+        if args[i] == "--scene" and args[i+1] then -- set the default scene with --scene
+            scene_manager.scene = args[i+1]
         end
 
-        if args[i] == "--dev" then dev = true end -- met le mode dev si ya l'arg "--dev" (une seule ligne omg)
+        if args[i] == "--dev" then dev = true end 
 
     end
 
-    scene_manager.scene = scene --application de la scene
 end
 
 return api 

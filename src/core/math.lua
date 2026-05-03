eb0524be0b5d583295f01e0e7b845c67506ab561
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-05-03 14:52:47 +0200

    less drawcall bc many block in one spot fixed and use cerp than lerp for cam and purple for debug text

--- src/core/math.lua
+++ src/core/math.lua
@@ -1,8 +1,15 @@
 local api = {}
 
+function api.cerp(a, b, t)
+    local f=(1-math.cos(t*math.pi)) 
+    return a*(1-f)+b*f 
+end
+
 function api.lerp(a, b, t)
     return a +(b - a) * t
 end
 
-
 return api
+
+
+

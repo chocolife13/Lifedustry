5d29f5ab9bc1aa9d52c5337181594d30c17811fe
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-05-03 10:03:12 +0200

    cleaning main.lua :bubbles:

--- main.lua
+++ main.lua
@@ -7,24 +7,16 @@ local args_manager = require("src.args_manager")
 -- THese 3 function are the 3 main function in LOVE2D
 
 function love.load(args) -- loading the game and retunr an arguments like --dev
+    debug.load()
     args_manager.start_scene(args)
-    if dev then
-        print("debug mod activated")
-    end
     assets.preload() -- loading the assets for the loading screen
     scene_manager.load() -- start the scene set in arg manager
 end
-
+ 
 
 function love.update(dt) --update all frames
     scene_manager.update(dt)
     screen.update()
-    if dev then
-        if love.keyboard.isDown("g") then
-            collectgarbage("collect")
-            print("garbage collected")
-        end
-    end
 end
 
 function love.draw() -- graphics

ab9d042f6e4102674c5d1dae165d9b51e072967b
SniperGeek <thymoec@gmail.com>
2026-05-06 11:46:18 +0200

    random seed at launch

--- src/map.lua
+++ src/map.lua
@@ -5,14 +5,14 @@ local screen = require("src.display.screen")
 
 
 local api = {}
-
+api.seed = love.math.random(0, 99999)
 function api.draw(x, y, zoom)
     local start_x = math.floor(x / 64)
     local start_y = math.floor(y / 64)
     for ix = (start_x - (screen.width / 2)/64)/zoom, (start_x + (screen.width / 2) / 64)/zoom do --cliping for x and y and btw 64 is thi tile dimmension 64x64
         for iy = (start_y - (screen.height / 2)/64)/zoom, (start_y + ((screen.height / 2)/64)+1)/zoom do
-            local height = love.math.noise(ix / 100, iy / 100) --gen a noise for height
-            local temp = love.math.noise(ix / 200, iy / 200) --gen a noise for height
+            local height = love.math.noise((ix + api.seed) / 100, (iy + api.seed) / 100) --gen a noise for height
+            local temp = love.math.noise((ix + api.seed) / 200, (iy + api.seed) / 200) --gen a noise for height
             --biome conditions
             if height < 0.1 then   
                 biome.water(ix, iy)

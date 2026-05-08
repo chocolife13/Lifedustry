2702a3978c1312c0551459ac80601e80f2297bd1
AaronTherapy <aarontherapyyt@gmail.com>
2026-05-08 23:50:54 +0200

    Add minimum window size

--- conf.lua
+++ conf.lua
@@ -3,6 +3,8 @@ function love.conf(t)
   t.window.icon = "/assets/textures/player.png" --window icon
 	t.window.width = 640	      
   t.window.height = 480
+  t.window.minwidth = 640 --minimum width of the window
+  t.window.minheight = 480 --minimum height of the window
   t.console = true -- activate the console
   t.window.resizable = true -- allow the windows to be resizable
   t.window.vsync = 0 -- V-Sync

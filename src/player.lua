84077ea6f3bac9664161d5153591a228872d30f5
AaronTherapy <aarontherapyyt@gmail.com>
2026-05-10 16:13:28 +0200

    Better player movement

--- src/player.lua
+++ src/player.lua
@@ -16,20 +16,25 @@ function api.draw()
 end
 
 function api.update(dt) 
-    if love.keyboard.isDown("up") then -- not better to put this in a input file or juste the up or down
-        api.velocity.y = -(api.speed * dt)
+    if love.keyboard.isDown("up") then
+        api.velocity.y = -api.speed
     end
     if love.keyboard.isDown("down") then
-        api.velocity.y = (api.speed*dt) --set the velocity to the speed
+        api.velocity.y = api.speed
     end
     if love.keyboard.isDown("left") then
-        api.velocity.x = -(api.speed*dt)
+        api.velocity.x = -api.speed
     end
     if love.keyboard.isDown("right") then
-        api.velocity.x = (api.speed*dt)
+        api.velocity.x = api.speed
     end
-    
-    
+
+    if api.velocity.x ~= 0 or api.velocity.y ~= 0 then --ajusting the speed values when both x and y movement happen
+        local length = math.sqrt(api.velocity.x^2 + api.velocity.y^2) --scary math
+        api.velocity.x = (api.velocity.x / length) * dt * api.speed -- formula to find the right value
+        api.velocity.y = (api.velocity.y / length) * dt * api.speed
+    end
+
     api.x = api.x + api.velocity.x --increse the x with the velocity
     api.y = api.y + api.velocity.y
     api.velocity.x = 0

ff77f97cbba050956204f9e155f76c8599a0b41e
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-05-10 10:10:07 +0200

    guys finaly

--- src/mobs.lua
+++ src/mobs.lua
@@ -1,32 +1,36 @@
 local assets = require("src.assets")
+local ui = require("src.display.ui")
+local math = require("src.core.math")
+local player = require("src.player")
 
 local api = {}
-api.x = 0
-api.y = 0
-api.timer = 0 -- Variable pour suivre le temps
+api.last = 0
 
--- Paramètres de l'onde
-local amplitude = 50  -- Hauteur de la vague (en pixels)
-local vitesse = 5    -- Vitesse de l'oscillation
-local y_central = 0 -- Position verticale de base
+
+function api.create(x, y)
+    api.last = api.last + 1
+    api[api.last] = {} 
+    api[api.last].x = x or 0
+    api[api.last].y = y or 0
+    api[api.last].name = name
+    api[api.last].type = type
+end
+
+api.create(nil)
+api.create(20, -200)
+
+
 
 function api.update(dt)
-    api.timer = api.timer + dt
-    
-    -- Avancement horizontal classique
-    api.x = api.x + 100 * dt
-    
-    -- Calcul du mouvement sinusoïdal
-    -- Formule : Position de base + (sin(temps * vitesse) * amplitude)
-    api.y = y_central + math.sin(api.timer * vitesse) * amplitude
+    api[1].x = math.lerp(api[1].x, player.x, dt)
+    api[2].x = api[2].x + dt * 100
 end
 
 function api.draw()
-    love.graphics.setColor(0, 0, 0, 0.5)
-    love.graphics.rectangle("fill", api.x-5, api.y-20, 30, 15)
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.print("bob", api.x, api.y-20)
-    love.graphics.draw(assets.textures.player, api.x, api.y)
+    for i=1, api.last  do
+        ui.print_centered(i, api[i].x, api[i].y -20)
+        love.graphics.draw(assets.textures.player, api[i].x, api[i].y)
+    end
 end
 
 return api

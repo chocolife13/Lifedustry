332073d92c8edc642190f302f1bf235f6128e9ad
Chocolife13 on vscode <carte.evolbie@gmail.com>
2026-05-14 15:48:10 +0200

    add entitiy count and t to tp to gilbert and u to tp all entityts new entity: snowman, snowball, fish, haters, chicken, ball

--- src/data/biome.lua
+++ src/data/biome.lua
@@ -1,4 +1,5 @@
 local blocks = require("src.data.blocks")
+local mobs = require("src.mobs")
 
 local biome = {}
 
@@ -8,6 +9,10 @@ local TILE = 64 -- tile size in pixels
 ---@param y integer  tile row
 function biome.plain(x, y)
     love.graphics.draw(blocks.grass.texture(), x * TILE, y * TILE)
+    if love.math.random(0, 500000) == 50 then
+		mobs.create(x * 64, y * 64, "","chicken")	
+	end
+    
 end
 
 ---@param x integer
@@ -20,18 +25,27 @@ end
 ---@param y integer
 function biome.desert(x, y)
     love.graphics.draw(blocks.sand.texture(), x * TILE, y * TILE)
+    if love.math.random(0, 500000) == 50 then
+		mobs.create((x * 64)-2000, y * 64, "","ball")	
+	end
 end
 
 ---@param x integer
 ---@param y integer
 function biome.water(x, y)
     love.graphics.draw(blocks.water.texture(), x * TILE, y * TILE)
+    if love.math.random(0, 50000) == 50 then
+		mobs.create(x * 64, y * 64, "","fish")	
+	end
 end
 
 ---@param x integer
 ---@param y integer
 function biome.snow(x, y)
     love.graphics.draw(blocks.snow.texture(), x * TILE, y * TILE)
+    if love.math.random(0, 500000) == 50 then
+		mobs.create(x * 64, y * 64, "","snowman")	
+	end
 end
 
 return biome
--- src/mobs.lua
+++ src/mobs.lua
@@ -2,6 +2,9 @@ local assets = require("src.assets")
 local ui = require("src.display.ui")
 local lmath = require("src.core.math")
 local player = require("src.player")
+local screen = require("src.display.screen")
+
+
 
 ---@class Mob
 ---@field x      number
@@ -10,11 +13,12 @@ local player = require("src.player")
 ---@field type   string
 ---@field timer  number
 ---@field goal   {x: number, y: number}
+---@field rotation number
 
 local mobs = {}
 
 ---@type Mob[]
-local list = {}
+mobs.list = {}
 
 --- Wandering logic
 function mobs.apply_wandering(mob, dt)
@@ -37,67 +41,106 @@ function mobs.apply_wandering(mob, dt)
     mob.timer = mob.timer - dt
 end
 
---- Creates a new mob and appends it to the Mob list.
+--- Creates a new mob and appends it to the Mob mobs.list.
 function mobs.create(x, y, name, mob_type)
     ---@type Mob
     local mob = {
         x = x or 0,
         y = y or 0,
-        name = name or (#list + 1),
+        name = name or (#mobs.list + 1),
         type = mob_type or "npc",
+        rotation =  rotation or 0,
         timer = 0,
         goal = {x = 0, y = 0}
     }
-    table.insert(list, mob)
+    table.insert(mobs.list, mob)
     return mob
 end
 
 -- The mobs added for testing ig
-mobs.create(20, -160, "Gilbert (Bro)", "folower")
+mobs.create(20, -160, "Gilbert (Bro)", "gilbert")
 mobs.create(nil, -160, "Bob", "statue")
 mobs.create(20, -160, "Random", "npc")
 mobs.create(0, -1000, "God of rocks", "god")
 
 function mobs.update(dt)
+
     -- Update movement for all NPCs
-    for _, mob in ipairs(list) do
-        if mob.type == "npc" then
-            mobs.apply_wandering(mob, dt)
-        end
-
-        if mob.type == "folower" then
-            mob.x = lmath.lerp(mob.x, player.x, 0.001)
-            mob.y = lmath.lerp(mob.y, player.y, 0.001)
-            
-        end
-        
-        if mob.type == "run" then
-            local run_speed = 300
-            local safe_distance = 300 -- Distance à laquelle il commence à avoir peur
-
-            -- Calcul de la distance
-            local dx = mob.x - player.x
-            local dy = mob.y - player.y
-            local distance = math.sqrt(dx * dx + dy * dy)
-
-            -- Si le joueur est trop proche, Gilbert s'enfuit
-            if distance < safe_distance and distance > 0 then
-                mob.x = mob.x + (dx / distance) * run_speed * dt
-                mob.y = mob.y + (dy / distance) * run_speed * dt
-            else
+    for _, mob in ipairs(mobs.list) do
+        local dx = mob.x - player.x
+        local dy = mob.y - player.y
+        local distance = math.sqrt(dx * dx + dy * dy)
+        if distance < 5000 then
+            if mob.type == "npc" or mob.type == "chicken" or mob.type == "fish" then
                 mobs.apply_wandering(mob, dt)
+            end
+            if mob.type == "snowman" then
+                mobs.apply_wandering(mob, dt)
+                mob.timer = mob.timer + 1 or 0
+                if mob.timer > 1000 then
+                    mobs.create(mob.x, mob.y, nil, "snowball")
+                    mob.timer = 0
+                end
+            end
+
+            if mob.type == "folower" then
+                mob.x = lmath.lerp(mob.x, player.x, 0.05)
+                mob.y = lmath.lerp(mob.y, player.y, 0.05)
+                
+            end
+            
+            if mob.type == "gilbert" then
+                mob.x =  mob.x + 1
+                
+                
+            end
+
+
+            if mob.type == "ball" then
+                mob.x = mob.x + 1
+                mob.rotation = mob.rotation + (dt *2)
+                
+            end
+            
+            if mob.type == "snowball" then
+                mob.x = lmath.lerp(mob.x, player.x, 0.001)
+                mob.y = lmath.lerp(mob.y, player.y, 0.001)
+                
+            end
+            
+            if mob.type == "run" then
+                local run_speed = 300
+                local safe_distance = 300 
+
+                
+                if distance < safe_distance and distance > 0 then
+                    mob.x = mob.x + (dx / distance) * run_speed * dt
+                    mob.y = mob.y + (dy / distance) * run_speed * dt
+                else
+                    mobs.apply_wandering(mob, dt)
+                end
             end
         end
     end
 end
 
 function mobs.draw()
-    for _, mob in ipairs(list) do
+    for _, mob in ipairs(mobs.list) do
         -- Draw name and texture
 	-- if api[i].x > player.x + (screen.width)/2 then -- try cliping
         ui.print_centered(tostring(mob.name), mob.x, mob.y - 20)
         if mob.type == "god" then
             love.graphics.draw(assets.textures.god, mob.x, mob.y)
+        elseif mob.type == "chicken" then
+            love.graphics.draw(assets.textures.chicken, mob.x, mob.y)
+        elseif mob.type == "fish" then
+            love.graphics.draw(assets.textures.fish, mob.x, mob.y)
+        elseif mob.type == "ball" then
+            love.graphics.draw(assets.textures.ball, mob.x, mob.y, mob.rotation)
+        elseif mob.type == "snowman" then
+            love.graphics.draw(assets.textures.snowman, mob.x, mob.y)
+         elseif mob.type == "snowball" then
+            love.graphics.draw(assets.textures.snowball, mob.x, mob.y)
         else
             love.graphics.draw(assets.textures.player, mob.x, mob.y)
         end

65bd4816de4fdb3d7ecbafa7fa3cdc68932827e2
chocolife13 <carte.evolbie@gmail.com>
2026-06-14 06:04:18 -0400

    comment how to use this fucking thing

--- android_launcher.sh
+++ android_launcher.sh
@@ -1,2 +1,4 @@
+# u need to allow "allow-external-apps = true" in ~/.termux/termux.properties
+# and termux-setup-storage
 zip -0 -r /storage/emulated/0/game.love *
 termux-open /storage/emulated/0/game.love

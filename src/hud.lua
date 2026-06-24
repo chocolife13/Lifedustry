3647e27750eec2ce33da8a876a5a9a9d466d0a55
chocolife13 <carte.evolbie@gmail.com>
2026-06-24 12:03:54 +0200

    hotbar system

--- src/hud.lua
+++ src/hud.lua
@@ -1,10 +1,9 @@
-local assets = require("src.assets")
-local screen = require("src.display.screen")
+local inventory = require("src.inventory")
 
 local hud = {}
 
 function hud.draw()
-    love.graphics.draw(assets.textures.inventory, screen.pct_x(50), screen.height - 20, 0, 1, 1, assets.textures.inventory:getWidth() / 2, assets.textures.inventory:getHeight())
+    inventory.draw()
 end
 
 return hud

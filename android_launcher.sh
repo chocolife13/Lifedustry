d4bd6553c17c1d787c56aae8a768348b688a23cd
chocolife13 <you@example.com>
2026-06-25 18:00:32 +0200

    init

--- android_launcher.sh
+++
@@ -1,4 +0,0 @@
-# u need to allow "allow-external-apps = true" in ~/.termux/termux.properties
-# and termux-setup-storage
-zip -0 -r /storage/emulated/0/game.love *
-termux-open /storage/emulated/0/game.love
--- src/hud.lua
+++
@@ -1,9 +0,0 @@
-local inventory = require("src.inventory")
-
-local hud = {}
-
-function hud.draw()
-    inventory.draw()
-end
-
-return hud

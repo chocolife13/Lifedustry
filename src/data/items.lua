6203b9a5cd3214910a88526406f153b3bb9598b9
chocolife13 <carte.evolbie@gmail.com>
2026-07-02 16:06:57 +0200

    apple do bell sound and the slingshot spawn infinite apple (2000)

--- src/data/items.lua
+++ src/data/items.lua
@@ -16,9 +16,24 @@ local items = {
             mobs.create((x + camera.x) - screen.width / 2, (y + camera.y) - screen.height / 2, "rock", "item")
         end
         },
-	apple = {name = "Apple", texture = assets.textures.apple},
+    apple = {
+        name = "Apple",
+        texture = assets.textures.apple,
+        onUse = function(x, y)
+        	assets.audios.sfx.bell:stop()
+        	assets.audios.sfx.bell:play()
+        end
+		},
     mushroom = { name = "mushroom", texture = assets.textures.mushroom },
     turnip = { name = "turnip", texture = assets.textures.turnip },
-    slingshot = {name = "slingshot", texture = assets.textures.slingshot}
+    slingshot = { name = "slingshot", texture = assets.textures.slingshot,
+    onUse = function(x, y)
+    	local mobs = require("src.mobs")
+        local camera = require("src.camera")
+        local screen = require("src.display.screen")
+        for i = 0, 2000 do
+        	mobs.create(((x  + camera.x) - screen.width / 2) + 10 * i, (y + camera.y) - screen.height / 2, "apple", "item")
+        end
+    end}
 }
 return items

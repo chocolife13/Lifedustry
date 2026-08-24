0b237d48935841aab9660e0b8ff53c8d8d405cef
chocolife13 <carte.evolbie@gmail.com>
2026-08-24 16:07:29 +0200

    online with serializer

--- src/multiplayer/client.lua
+++ src/multiplayer/client.lua
@@ -2,6 +2,25 @@ local client = {}
 local enet = require "enet"
 local map = require("src.map")
 
+
+local function deserialize(str)
+    local tbl = {}
+    for k, v in str:gmatch("([^;=]+)=([^;=]+)") do
+        tbl[k] = tonumber(v) or v
+    end
+    return tbl
+end
+
+local function serialize(tbl)
+    local result = {}
+    for k, v in pairs(tbl) do
+        table.insert(result, k .. "=" .. tostring(v))
+    end
+    return table.concat(result, ";")
+end
+
+
+
 function client.connect(ip)
     client.host = enet.host_create()
     client.server = client.host:connect(ip .. ":9111")
@@ -12,9 +31,9 @@ function client.update()
     if client.event then
         if client.event.type == "receive" then
             --print("Got message: ", client.event.data, client.event.peer)
-            local data = client.event.data
-            print(data)
-            map.seed = tonumber(data)
+            local data = deserialize(client.event.data)
+            print(data.seed)
+            map.seed = tonumber(data.seed)
         end
     end
 end
--- src/multiplayer/server.lua
+++ src/multiplayer/server.lua
@@ -1,10 +1,29 @@
 local enet = require "enet"
 local host = enet.host_create("0.0.0.0:9111")
 print("server starting at 0.0.0.0:9111")
+print("seed = 123")
+local function deserialize(str)
+    local tbl = {}
+    for k, v in str:gmatch("([^;=]+)=([^;=]+)") do
+        tbl[k] = tonumber(v) or v
+    end
+    return tbl
+end
+
+local function serialize(tbl)
+    local result = {}
+    for k, v in pairs(tbl) do
+        table.insert(result, k .. "=" .. tostring(v))
+    end
+    return table.concat(result, ";")
+end
+
+
 while true do
   local event = host:service(100)
   while event do
-    event.peer:send("123") --seed
+    local seed = 123
+    event.peer:send(serialize({seed = 123})) --seed
     if event.type == "connect" then
       print(event.peer, "is connected")
     end

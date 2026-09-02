6df37fcf6e5c5b54618a961f56e3610eb0ae5aa3
chocolife13 <carte.evolbie@gmail.com>
2026-09-02 19:04:12 +0200

    fix crash and comment the code

--- src/multiplayer/server.lua
+++ src/multiplayer/server.lua
@@ -1,11 +1,9 @@
+-- libs 
 local enet = require "enet"
-local host = enet.host_create("0.0.0.0:9111")
-print("server starting at 0.0.0.0:9111")
-print("seed = 123")
 
-server = {}
-server.player = {}
 
+
+-- function for data
 local function deserialize(str)
     local tbl = {}
     for k, v in str:gmatch("([^;=]+)=([^;=]+)") do
@@ -23,29 +21,48 @@ local function serialize(tbl)
 end
 
 
-while true do
-  local event = host:service(100)
+
+
+
+-- set the server
+local host = enet.host_create("0.0.0.0:9111") -- set the target of data to 0.0.0.0 (like all adresse) and port 9111
+server = {}
+server.player = {}
+
+
+
+
+-- starting print
+print("server starting at 0.0.0.0:9111")
+print("seed = 123")
+
+
+
+
+
+
+while true do -- holy infinte loop
+  local event = host:service(100) -- time in ms to wait ?
   while event do
-    local seed = 123
-    event.peer:send(serialize({seed = 123}))
-     if event then
-        if event.type == "receive" then
+    local seed = 123 -- seed to sent for test
+    
+     if event then -- if new thing on serv
+
+        if event.type == "connect" then
+            event.peer:send(serialize({"seed" = 123})) -- send to the new player the seed
+            server.player[event.peer] = {} -- make a table 4 the new player
+        end
+        
+        if event.type == "receive" then -- if new info in serv like pos and all
             local data = deserialize(event.data)
-            server.player[event.peer] = {x = data.x, y = data.y}
-            for player, _ in pairs(server.player) do
-                for _, _ in pairs(server.player[player]) do
-                    print("player = " .. tostring(player), i, v)
-                end
+            if server.player[event.peer] then
+                server.player[event.peer] = {x = data.x, y = data.y}
+            else
+                print("Who is" .. event.peer "🥀")
             end
         end
     end 
-    if event.type == "connect" then
-        --print("players: ")
-        table.insert(server.player, event.peer)
-        for i, v in pairs(server.player) do
-            print("players" .. i)
-        end
-    end
+    
     event = host:service()
   end
 end

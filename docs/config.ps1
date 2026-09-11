b2af7085398f078ec8ded9e66fd91e1b87b51fa4
Lenny <119404554+chocolife13@users.noreply.github.com>
2026-09-11 10:06:36 +0200

    Add PowerShell script for project setup

---
+++ docs/config.ps1
@@ -0,0 +1,6 @@
+Clear-Host
+Set-Location ./Lifedustry
+Set-Alias -Name love -Value ../love/love.exe
+Set-Alias -Name git -Value ..\git\cmd\git.exe
+Write-Host "love ./ for launch normal" -ForegroundColor Magenta
+Write-Host "love ./ --dev for dev launch" -ForegroundColor blue

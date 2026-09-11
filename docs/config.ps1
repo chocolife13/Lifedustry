19b4718f8bdd6e295eaf4ad7e89792bf73cf1bbc
chocolife13 <carte.evolbie@gmail.com>
2026-09-11 19:16:00 +0200

    automatic script - download git - clone lifedustry - add a shell script and indication

--- docs/config.ps1
+++ docs/config.ps1
@@ -1,6 +1,34 @@
 Clear-Host
-Set-Location ./Lifedustry
+
+$ascii = @'
+ ____                _           _              
+|  _ \  _____   ____| |_   _ ___| |_ _ __ _   _ 
+| | | |/ _ \ \ / / _` | | | / __| __| '__| | | |
+| |_| |  __/\ V / (_| | |_| \__ \ |_| |  | |_| |
+|____/ \___| \_/ \__,_|\__,_|___/\__|_|   \__, |
+                                          |___/ 
+'@
+Write-Host $ascii -ForegroundColor Green
+Write-Host "The dev tool for lifedustry !" -ForegroundColor Blue
+Write-Host " "
+Write-Host "Starting Lifedustry #############################################" -ForegroundColor Red
+Write-Host '"love ./ --dev" for stating as dev' -ForegroundColor Magenta
+Write-Host '"love ./ --scene "scene"" for stating to the wanted scene' -ForegroundColor Magenta
+Write-Host "love ./ for start normaly" -ForegroundColor Magenta
+Write-Host ""
+Write-Host "Manage version update and push ##################################" -ForegroundColor Red
+Write-Host '"git pull" for pulling the latest version' -ForegroundColor Magenta
+Write-Host '"git add" ./* for add all file to commit ' -ForegroundColor Magenta
+Write-Host '"git commit" -m "ur message XD" for save modification' -ForegroundColor Magenta
+Write-Host '"git push" to send modificatoin' -ForegroundColor Magenta
+Write-Host ''
+
+if (Test-Path "./Lifedustry") {
+	Write-Host 'Lifedustry installed' -ForegroundColor Green
+    Set-Location ./Lifedustry
+} else {
+	Write-Host 'Lifedustry not installed type "git clone https://github.com/chocolife13/Lifedustry.git"' -ForegroundColor Red
+}
+
 Set-Alias -Name love -Value ../love/love.exe
 Set-Alias -Name git -Value ..\git\cmd\git.exe
-Write-Host "love ./ for launch normal" -ForegroundColor Magenta
-Write-Host "love ./ --dev for dev launch" -ForegroundColor blue
--- docs/lifedustry_automatic_setup.bat
+++ docs/lifedustry_automatic_setup.bat
@@ -2,61 +2,40 @@
 title Lifedustry portable setup win64 edition
 
 echo This script will download Lifedustry and Love2D and somes scripts 
-echo in this script folder in %cd% 
+echo in %cd% 
 echo if you want to continue press enter else close this windows
 pause
 cls
 
 
-
-echo ⚡ love2D downloading...
-echo ⌛ Lifedustry
-echo ⌛ Scriptes
-echo #######################################
+echo downloading love2D
 
 for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-RestMethod 'https://api.github.com/repos/love2d/love/releases/latest').assets | Where-Object {$_.name -like '*win64.zip'} | Select-Object -ExpandProperty browser_download_url"') do set URL=%%i
 curl -L -o love.zip "%URL%"
 powershell -NoProfile -Command "Expand-Archive 'love.zip' '.'"
 del love.zip
 for /d %%d in (love-*-win64) do ren "%%d" love
-cls
+
  
 
+echo downloading git
+for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-RestMethod 'https://api.github.com/repos/git-for-windows/git/releases/latest').assets | Where-Object {$_.name -match 'MinGit.*-64-bit\.zip$'} | Select-Object -ExpandProperty browser_download_url"') do set GIT_URL=%%i
+curl -L -o git.zip "%GIT_URL%"
+mkdir Git
+powershell -NoProfile -Command "Expand-Archive 'git.zip' './Git'"
+del git.zip
 
 
 
 
 
-
-echo ✅ love2D downloaded
-echo ⚡ Lifedustry downloading..
-echo ⌛ Scriptes
-echo #######################################
-
-curl -L -o Lifedustry.zip https://github.com/chocolife13/Lifedustry/archive/refs/heads/main.zip
-powershell -NoProfile -Command "Expand-Archive 'Lifedustry.zip' '.'"
-del Lifedustry.zip
-if exist Lifedustry-main ren Lifedustry-main Lifedustry
-cls
+echo downloading Lifedustry
+"./git/cmd/git.exe" clone https://github.com/chocolife13/Lifedustry.git
 
 
 
-
-echo ✅ love 2D downloaded
-echo ✅ Lifedustry downloaded
-echo ⚡ Scriptes downloading...
-echo #######################################
-
-
-echo start ./love/love.exe ./Lifedustry/ > run.bat
-echo start ./love/love.exe ./Lifedustry/ --dev > run_dev.bat
-cls
-
-echo ✅ love2D downloaded
-echo ✅ Lifedustry downloaded
-echo ✅ Scriptes downloaded
-echo #######################################
-echo Finish
+echo creating scripts
+curl -L -o shell.bat "https://raw.githubusercontent.com/chocolife13/Lifedustry/refs/heads/main/docs/shell.bat"
+curl -L -o config.ps1 "https://raw.githubusercontent.com/chocolife13/Lifedustry/refs/heads/main/docs/config.ps1"
 pause
-
 

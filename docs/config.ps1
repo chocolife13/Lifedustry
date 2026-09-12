7a6e7443fcf7f985ec78f1275289e440926966c8
chocolife13 <carte.evolbie@gmail.com>
2026-09-12 15:34:45 +0200

    add ghcli

--- docs/config.ps1
+++ docs/config.ps1
@@ -9,26 +9,33 @@ $ascii = @'
                                           |___/ 
 '@
 Write-Host $ascii -ForegroundColor Green
-Write-Host "The dev tool for lifedustry !" -ForegroundColor Blue
+Write-Host "The dev tool for lifedustry !" -ForegroundColor blue
 Write-Host " "
-Write-Host "Starting Lifedustry #############################################" -ForegroundColor Red
-Write-Host '"love ./ --dev" for stating as dev' -ForegroundColor Magenta
-Write-Host '"love ./ --scene "scene"" for stating to the wanted scene' -ForegroundColor Magenta
-Write-Host "love ./ for start normaly" -ForegroundColor Magenta
+Write-Host "Starting Lifedustry #############################################" -ForegroundColor DarkYellow
+Write-Host '"love ./ --dev" for stating as dev' -ForegroundColor Blue
+Write-Host '"love ./ --scene "scene"" for stating to the wanted scene' -ForegroundColor Blue
+Write-Host '"love ./" for start normaly' -ForegroundColor Blue
 Write-Host ""
-Write-Host "Manage version update and push ##################################" -ForegroundColor Red
-Write-Host '"git pull" for pulling the latest version' -ForegroundColor Magenta
-Write-Host '"git add" ./* for add all file to commit ' -ForegroundColor Magenta
-Write-Host '"git commit" -m "ur message XD" for save modification' -ForegroundColor Magenta
-Write-Host '"git push" to send modificatoin' -ForegroundColor Magenta
+Write-Host "Manage version update and push ##################################" -ForegroundColor DarkYellow
+Write-Host 'First you need to login to github type "gh auth login"' -ForegroundColor green
+Write-Host '"git pull" for pulling the latest version' -ForegroundColor Blue
+Write-Host '"git add" ./* for add all file to commit ' -ForegroundColor Blue
+Write-Host '"git commit" -m "ur message XD" for save modification' -ForegroundColor Blue
+Write-Host '"git push" to send modificatoin' -ForegroundColor Blue
 Write-Host ''
+
+
 
 if (Test-Path "./Lifedustry") {
 	Write-Host 'Lifedustry installed' -ForegroundColor Green
     Set-Location ./Lifedustry
 } else {
-	Write-Host 'Lifedustry not installed type "git clone https://github.com/chocolife13/Lifedustry.git"' -ForegroundColor Red
+	Write-Host 'Lifedustry not installed ' -ForegroundColor Red
+    Write-Host 'if you are collaborator type "git clone https://github.com/chocolife13/Lifedustry.git" ' -ForegroundColor Red
+    Write-Host 'else "gh repo fork chocolife13/Lifedustry --clone" for cloning lifedustry on ur github ' -ForegroundColor Red
+   
 }
 
-Set-Alias -Name love -Value ../love/love.exe
-Set-Alias -Name git -Value ..\git\cmd\git.exe
+$env:PATH += ";$PWD\Git\cmd"
+$env:PATH += ";$PWD\githubcli\bin"
+$env:PATH += ";$PWD\love"
--- docs/lifedustry_automatic_setup.bat
+++ docs/lifedustry_automatic_setup.bat
@@ -1,8 +1,7 @@
 @echo off
 title Lifedustry portable setup win64 edition
 
-echo This script will download Lifedustry and Love2D and somes scripts 
-echo in %cd% 
+echo This script will download and Love2D git and github cli just in the %cd% folder
 echo if you want to continue press enter else close this windows
 pause
 cls
@@ -16,7 +15,13 @@ powershell -NoProfile -Command "Expand-Archive 'love.zip' '.'"
 del love.zip
 for /d %%d in (love-*-win64) do ren "%%d" love
 
- 
+
+echo downloading GitHub CLI
+for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-RestMethod 'https://api.github.com/repos/cli/cli/releases/latest').assets | Where-Object {$_.name -like '*windows_amd64.zip'} | Select-Object -ExpandProperty browser_download_url"') do set GH_URL=%%i
+curl -L -o gh.zip "%GH_URL%"
+mkdir githubcli
+powershell -NoProfile -Command "Expand-Archive 'gh.zip' './githubcli'"
+del gh.zip
 
 echo downloading git
 for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-RestMethod 'https://api.github.com/repos/git-for-windows/git/releases/latest').assets | Where-Object {$_.name -match 'MinGit.*-64-bit\.zip$'} | Select-Object -ExpandProperty browser_download_url"') do set GIT_URL=%%i
@@ -26,15 +31,7 @@ powershell -NoProfile -Command "Expand-Archive 'git.zip' './Git'"
 del git.zip
 
 
-
-
-
-echo downloading Lifedustry
-"./git/cmd/git.exe" clone https://github.com/chocolife13/Lifedustry.git
-
-
-
-echo creating scripts
+echo Download scripts and tools
 curl -L -o shell.bat "https://raw.githubusercontent.com/chocolife13/Lifedustry/refs/heads/main/docs/shell.bat"
 curl -L -o config.ps1 "https://raw.githubusercontent.com/chocolife13/Lifedustry/refs/heads/main/docs/config.ps1"
 pause

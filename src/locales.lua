81c887d212d7f1c1ef7e5bb19a5a9b366f87ff98
chocolife13 <carte.evolbie@gmail.com>
2026-09-13 16:12:39 +0200

    auto language

--- src/locales.lua
+++ src/locales.lua
@@ -1,6 +1,11 @@
 locales = {} --public
-local lang = "en"
 
+
+local lang = select(2, pcall(os.setlocale, "", "all"))
+lang = lang:match("([a-z]+)") or "en"
+
+
+print(languageCode)
 function locales.load()
     locales.texts = require("src.data.locales." .. lang)
 end
